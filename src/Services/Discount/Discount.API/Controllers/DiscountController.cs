d6456dc5f8502f2c9edace2d5840229053c11c8e
Ataberk Dağdelen <ataberk.dagdelen@gmail.com>
2022-03-06 15:38:46 +0300

    Discount API added. Discount API dockerized.

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,54 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            return Ok(await this._repository.GetDiscount(productName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<ActionResult<bool>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            var result = await this._repository.CreateDiscount(coupon);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<ActionResult<bool>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            var result = await this._repository.UpdateDiscount(coupon);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            var result = await this._repository.DeleteDiscount(productName);
+
+            return Ok(result);
+        }
+    }
+}
